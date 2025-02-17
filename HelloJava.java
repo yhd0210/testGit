508263265ac2dcdbb838344e36c8c22990aa2db7
이유나 <jee1358@naver.com>
2025-02-17 16:14:12 +0900

    git1 - 수정

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
     }
-    class static void git1() {
+    public static void git1() {
         System.out.println("git ");
     }
 }
