44938764e916e48c10230a6db85d27c5483d5a39
ryu <yhd060210@gmail.com>
2025-02-17 15:46:20 +0900

    Introduce_Method

--- HelloJava.java
+++ HelloJava.java
@@ -2,4 +2,7 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
     }
+    public static void introduce () {
+        System.out.println("안녕하세요! 홍길동입니다.");
+    }
 }
